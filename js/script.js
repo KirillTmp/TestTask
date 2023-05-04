353e9821a169dc3400f0af3085bf3594d54333c8
KirillTmp <132596478+KirillTmp@users.noreply.github.com>
2023-05-04 20:17:24 +0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function ChangeImgFood()
+{
+    const myImg = document.getElementById('food');
+    myImg.src = '/img/Food menu.png';
+}
+function ChangeImgDrinks()
+{
+    const myImg = document.getElementById('food');
+    myImg.src = '/img/Drinks menu.png';
+}
+function ChangeImgTakeaway()
+{
+    const myImg = document.getElementById('food');
+    myImg.src = '/img/Takeaway menu.png';
+}
